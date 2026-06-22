97e6402623cca9367d6aa3814d0ceaefe8b9fb62
Khushi Sharma <khushi9084735022@gmail.com>
2026-06-22 15:26:28 +0530

    fix

--- provider.tf
+++ provider.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_version = ">= 1.5"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-}
-
 provider "aws" {
   region = var.aws_region
   access_key = var.aws_access_key_id
