f382b8bedadde3c0663fcb701db3d77b8311111a
Khushi Sharma <khushi9084735022@gmail.com>
2026-06-22 11:20:23 +0530

    update code

--- main.tf
+++ main.tf
@@ -1,1 +1,25 @@
 
+resource "aws_instance" "web" {
+
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = var.instance_type
+
+  subnet_id = aws_subnet.public.id
+  key_name = var.key
+
+  vpc_security_group_ids = [
+    aws_security_group.ec2_sg.id
+  ]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y nginx
+              systemctl enable nginx
+              systemctl start nginx
+              EOF
+
+  tags = {
+    Name = "ngnix-server"
+  }
+}
