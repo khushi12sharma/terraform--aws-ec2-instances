f4126c0815fd0f410cf2677014a20baeae706bc4
Khushi Sharma <khushi9084735022@gmail.com>
2026-06-22 11:26:19 +0530

    update variable

--- variable.tf
+++ variable.tf
@@ -1,21 +1,17 @@
 variable "aws_region" {
-  default = "ap-south-1"
+    type = string
 }
 
 variable "vpc_cidr" {
-  default = "10.0.0.0/16"
+    type = string
 }
 
 variable "public_subnet_cidr" {
-  default = "10.0.1.0/24"
+    type = string
 }
 
 variable "private_subnet_cidr" {
-  default = "10.0.2.0/24"
-}
-
-variable "instance_type" {
-  default = "t2.micro"
+    type = string
 }
 
 variable "key" {
