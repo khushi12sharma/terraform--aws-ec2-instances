2d21cc01971831f7f913f781b10d6a9446306f1b
Khushi Sharma <khushi9084735022@gmail.com>
2026-06-22 11:45:32 +0530

    update variable

--- variable.tf
+++ variable.tf
@@ -1,3 +1,14 @@
+variable "aws_access_key_id" {
+  description = "AWS Access Key ID"
+  type        = string
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS Secret Access Key"
+  type        = string
+  sensitive   = true  # Mark as sensitive so it's not displayed in the logs
+}
+
 variable "aws_region" {
     type = string
 }
